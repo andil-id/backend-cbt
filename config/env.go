3fcdf88305fb7ddb0ea8c513ff8396ae44cc39c2
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-11-11 17:09:52 +0700

    feat: add GinMode, and pathLog

--- config/env.go
+++ config/env.go
@@ -2,6 +2,8 @@ package config
 
 import "os"
 
+var GinMode = os.Getenv("GIN_MODE")
+var PathLog = os.Getenv("PATH_LOG")
 var CloudinaryCloudName = os.Getenv("CLOUDINARY_CLOUD_NAME")
 var CloudinaryApiKey = os.Getenv("CLOUDINARY_API_KEY")
 var CloudinaryApiSecreet = os.Getenv("CLOUDINARY_API_SECRET")
